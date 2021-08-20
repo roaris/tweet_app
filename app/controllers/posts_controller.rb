9b57187f6c2ddb9ef68f41df270bf8629a70ca81
roaris <one0803kyopro@gmail.com>
2021-08-20 23:59:00 +0900

    投稿詳細ページを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,9 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    @id = params[:id]
+    @post = Post.find_by(id: @id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/' => 'home#top'
   get 'about' => 'home#about'
   get 'posts/index' => 'posts#index'
+  get 'posts/:id' => 'posts#show'
 end
