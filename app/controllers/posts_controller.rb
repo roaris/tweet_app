d0a5060fb73712b774283b39bcde9e15c6854f14
roaris <one0803kyopro@gmail.com>
2021-08-21 00:14:40 +0900

    投稿の新規作成を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,13 @@ class PostsController < ApplicationController
     @id = params[:id]
     @post = Post.find_by(id: @id)
   end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to('/posts/index')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get '/' => 'home#top'
   get 'about' => 'home#about'
   get 'posts/index' => 'posts#index'
+  get 'posts/new' => 'posts#new'
   get 'posts/:id' => 'posts#show'
+  post 'posts/create' => 'posts#create'
 end
