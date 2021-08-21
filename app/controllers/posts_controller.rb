89cd7ca3af7f50d5f42ed177087c297a62e3c1a0
roaris <one0803kyopro@gmail.com>
2021-08-22 03:19:12 +0900

    投稿の編集機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,18 @@ class PostsController < ApplicationController
   def new
   end
 
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to('/posts/index')
+
+  end
+
   def create
     @post = Post.new(content: params[:content])
     @post.save
