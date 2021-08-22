5a2397a77b64be080427cb907a260e62f1662228
roaris <one0803kyopro@gmail.com>
2021-08-22 23:42:06 +0900

    プロフィール画像についての実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_22_131946) do
+ActiveRecord::Schema.define(version: 2021_08_22_141643) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_08_22_131946) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_name"
   end
 
 end
