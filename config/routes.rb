61f58eb486e9e40caf7aa0a7f0b50df557f5e6e6
roaris <one0803kyopro@gmail.com>
2021-08-23 00:41:54 +0900

    ログイン・ログアウト機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,7 @@ Rails.application.routes.draw do
   post 'users/create' => 'users#create'
   get 'users/:id/edit' => 'users#edit'
   post 'users/:id/update' => 'users#update'
+  get 'login' => 'users#login_form'
+  post 'login' => 'users#login'
+  post 'logout' => 'users#logout'
 end
